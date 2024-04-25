b26f1e4cf46cebde0efc6bc8588e50f8ff0ea6b0
Nils Durieux <nils.durieux@etu.u-bordeaux.fr>
2024-04-25 12:20:04 +0200

    Refactor + Documentation

--- modules/utils.js
+++ modules/utils.js
@@ -1,14 +1,32 @@
 export class Utils{
-    static zoomSpeed = 0.1;
-
+    /**
+     * Waits a certain amount of milliseconds, in the form of a promise.
+     * @param {*} ms Time in milliseconds to wait for.
+     * @returns The timeout promise.
+     */
     static sleep = (ms) => new Promise(r => setTimeout(r, ms));
 
-    static displayBoolean = (bool) => {return bool ? 'Yes' : 'No'}
+    /**
+     * Returns a text depending on a state.
+     * @param {*} bool Any boolean
+     * @returns Yes if true, No otherwise
+     */
+    static displayBoolean = (bool) => bool ? 'Yes' : 'No';
 
+    /**
+     * Returns seconds from time in milliseconds.
+     * @param {*} ms Time in milliseconds
+     * @returns Seconds
+     */
     static getSecondsFromMilliseconds = (ms) => {
         return Math.floor(ms/1000);
     }
 
+    /**
+     * Formats the date.
+     * @param {*} date Date in yyyy/mm/dd format. 
+     * @returns Same date in dd/mm/yyyy format.
+     */
     static formatDate = (date) => {
         let newdate = date.substring(0,10);
         newdate = newdate.split('-').reverse();
@@ -16,6 +34,11 @@ export class Utils{
         return `${d}/${m}/${y}`;
     }
     
+    /**
+     * Formats the time.
+     * @param {*} date Time in hh/mm/ss
+     * @returns Time in hh:mm:ss
+     */
     static formatTime = (date) => {
         let newtime = date.substring(11,19);
         newtime = newtime.split(':');
