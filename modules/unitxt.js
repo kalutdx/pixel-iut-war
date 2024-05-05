71d527eeb471a738682c25fec8845501318b6ee3
Nils Durieux <nils.durieux@etu.u-bordeaux.fr>
2024-05-05 16:50:57 +0200

    Unitxt update

--- modules/unitxt.js
+++ modules/unitxt.js
@@ -1,14 +1,14 @@
 /*This class's name is a reference to the file that handles all texts in Phantasy Star Online Blue Burst.*/
 
 export class Unitxt{
-    /*------ INFOBOX ------*/    
-    static info = 'Info';
-    static warning = 'Warning';
-    static error = 'Error';
+    /*------ BUTTONS ------*/
+    static saveButton = 'Save';
+    static deleteButton = 'Delete';
 
-    static unknownError = 'An unknown error has occured.'
+    /*------ INFOBOX ------*/  
+    static unknownError = 'An unknown error has occured.';
     
-    static unknownUID = 'Unknown user. Please enter a correct UID and try again.'
+    static unknownUID = 'Unknown user. Please enter a correct UID and try again.';
 
     static missingTeam = 'You don\'t belong to any team. Go pick one !';
     static teamJoinError = 'An error has occured when attempting to join a team.';
@@ -21,6 +21,13 @@ export class Unitxt{
     static UIDBrowserSecurityError = (isSave) => `Can\'t ${isSave ? 'save':'load'} your UID. Check your browser settings.`;
 
     static ThemeSaveSuccessful = 'Successfully saved your theme.';
-    static ThemeChangeSuccessful = 'Theme changed.'
+    static ThemeChangeSuccessful = 'Theme changed.';
     static ThemeSecurityError = (isSave) => `Can\'t ${isSave ? 'save':'load'} your theme. Check your browser settings.`;
+
+    /*------ THEMES ------*/
+    static option = '> Custom';
+
+    static mainColor = 'Main';
+    static backColor = 'Back';
+    static detailsCOlor = 'Details';
 }
