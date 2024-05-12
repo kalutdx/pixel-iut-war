3db908c95a8dfbb1a0d53cdb17e0cd09553c78e9
Nils Durieux <nils.durieux@etu.u-bordeaux.fr>
2024-05-12 11:01:57 +0200

    Removed theme feature + README.md

--- modules/app.js
+++ modules/app.js
@@ -3,7 +3,6 @@ import { Texts } from "./texts.js";
 import { Utils } from "./utils.js";
 import { Infobox } from "./infobox.js";
 import { Unitxt } from "./unitxt.js";
-import { Theme } from "./theme.js";
 
 // ------ GETTERS ------
 
@@ -273,29 +272,12 @@ document.getElementById("team-4-selection").addEventListener("click", (event)=>{
     teamSelect(user, 4);
 })
 
-/*- Theme change -*/
-document.getElementById("theme-selector").addEventListener("change", (event)=>{
-    const selectedElement = event.target;
-    const val = selectedElement.value;
-    console.log(val);
-    if(val === Unitxt.option){
-        Theme.displayCustomOption(true);
-    } else {
-        Theme.displayCustomOption(false);
-        const theme = Theme.themes.get(val);
-        theme.applyTheme();
-    }
-})
 
 // ------ MAIN ------
 
 const main = async () => {
     // Loading UID
     loadUid();
-    // Loading themes 
-    Theme.addDefaultThemes();
-    Theme.loadAllThemesToMenu();
-    Theme.addCustomOption();
     // Main loop
     while(true){
         displayGrid();
